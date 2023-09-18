624ee5bbec68d7eb99d741da5fa2c7b124f79261
bahaddin.ahmadov@limestonedigital.com <bahaddin.ahmadov@limestonedigital.com>
2023-09-18 19:50:05 +0400

    start

---
+++ src/components/UserList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>User List</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: []
+    };
+  },
+  async created() {
+    try {
+      const response = await fetch('http://localhost:7799/api/users', {
+        headers: {
+          'accept': '*/*'
+        }
+      });
+      this.users = await response.json();
+    } catch (error) {
+      console.error("There was an error fetching the users:", error);
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Add any styles specific to this component here */
+</style>
